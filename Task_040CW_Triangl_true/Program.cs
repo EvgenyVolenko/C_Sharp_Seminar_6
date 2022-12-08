38bf04e2e74af21de570c2ad7a091f81b353eb0e
Evgeny <evolenko@inbox.ru>
2022-12-08 21:39:54 +0300

    Added  Task040

---
+++ Task_040CW_Triangl_true/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 40: Напишите программу, которая принимает на вход три числа и проверяет, 
+// может ли существовать треугольник с сторонами такой длины.
+// Теорема о неравенстве треугольника: каждая сторона треугольника меньше суммы двух других сторон.
+
+Console.Clear();
+int a, b, c;
+bool pr;
+
+a = vvod("A");
+b = vvod("B");
+c = vvod("C"); 
+
+pr = Proverka(a, b, c);
+if ( pr == true) Console.Write("Треугольник с указанными сторонами существует.");
+else Console.Write("Треугольник с указанными сторонами НЕ существует.");
+
+bool Proverka(int first, int second, int fird)
+{
+    if (first < second + fird && second < first + fird && fird < first + second) 
+    {
+        return true;
+    }
+    else return false;
+}
+
+int vvod(string storona)
+{
+    Console.Write($"Введите длинну стороны {storona} треугольника: ");
+    int digit;
+    while (true)
+    {
+        if (int.TryParse(Console.ReadLine(), out digit)) break;
+        else Console.Write("Ну просил же ЧИСЛО: ");
+    }
+    return digit;
+}
